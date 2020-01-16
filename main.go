be59563ad59dc7590beacde0ac66489403cf2073
Jacob Jenner Rasmussen <jacob.mindstorms@gmail.com>
2020-01-16 13:44:02 +0100

    Added endpoint to clear all tokens
    Sending a GET request to /wipe removes all tokens.
    Token wiping need to be enabled by setting the ENABLE_WIPING environment
    variable.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 
 	_ "github.com/npflan/steam-gameserver-token-api/steam"
 )
@@ -17,6 +18,12 @@ func mayEnv(envVar string, deflt string) string {
 }
 
 func main() {
+	enableWiping, err := strconv.ParseBool(mayEnv("ENABLE_WIPING", "false"))
+	if err != nil {
+		fmt.Println("Invalid value specified for ENABLE_WIPING")
+		os.Exit(1)
+	}
+
 	a := App{}
-	a.Run(mayEnv("STEAM_WEB_API_BIND_ADDRESS", ":8000"))
+	a.Run(mayEnv("STEAM_WEB_API_BIND_ADDRESS", ":8000"), enableWiping)
 }
